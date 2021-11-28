d04d7be250679f2b4ab185cc1d0226cc1372a794
domoszlai <dlacko@gmail.com>
2021-11-28 22:00:26 +0100

    Add test script

---
+++ tests/test-svg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TEST_SVGS="../resources/tests/*.svg"
+for svg in $TEST_SVGS
+do
+  echo -n "Testing $svg file... "
+  juicy-gcode.exe $svg | cmp -s ${svg%.svg}.gcode -
+  if [ $? -eq 0 ]
+  then
+   echo "ok"
+  else
+   echo "failed" 
+  fi  
+done
