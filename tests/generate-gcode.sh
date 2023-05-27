3946761f6deb270a421cc48ec1177a9c7c016926
Laszlo Domoszlai <dlacko@gmail.com>
2023-05-27 20:25:52 +0200

    Add colored test cases

--- tests/generate-gcode.sh
+++ tests/generate-gcode.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
 TEST_SVGS="../resources/tests/*.svg"
 for svg in $TEST_SVGS
 do
   echo -n "Processing $svg file... "
   juicy-gcode -o ${svg%.svg}.gcode $svg
   juicy-gcode -c linear -o ${svg%.svg}.linear.gcode $svg
+  echo "ok" 
+done
+
+TEST_COLOR_SVGS="../resources/tests/color/*.svg"
+for svg in $TEST_COLOR_SVGS
+do
+  echo -n "Processing $svg file... "
+  juicy-gcode -f "../resources/tests/color/flavor.yml" -o ${svg%.svg}.gcode $svg
   echo "ok" 
 done
